bf5233c32ac4a8688e9659da8c1d5bd5c2173fdc
mendes <jrmmendes@outlook.com>
2025-09-15 20:44:07 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ReceiptsController } from './application/receipts.controller';
+import { QueueClient } from './infra/queue/queue.client';
+import { MockQueueAdapter } from './infra/queue/adapters/mock-queue.adapter';
+import { LoggerModule } from 'nestjs-pino';
+import { pinoHttp } from './infra/logger/logger.config';
+import { IngestReceiptCommand } from './domain/ingest-fiscal-receipt.command';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: pinoHttp,
+    }),
+  ],
+  controllers: [ReceiptsController],
+  providers: [
+    IngestReceiptCommand,
+    {
+      provide: QueueClient,
+      useClass: MockQueueAdapter,
+    },
+  ],
 })
 export class AppModule {}
