6238b09faa3a5f108d8297ca5f9751597bd45e40
mendes <jrmmendes@outlook.com>
2025-09-15 21:35:18 -0300

    test: add jest config

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require('./tsconfig.json')
+
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/tests/**/*.spec.ts',
+  ],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>',
+    }),
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.(module|config).ts',
+    '!<rootDir>/src/(handler|main|application).ts',
+    '!<rootDir>/src/**/index.ts',
+  ],
+  coverageDirectory: './coverage',
+  fakeTimers: {
+    enableGlobally: false,
+  }
+}
